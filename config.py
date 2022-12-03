7ca19dd7ad57ee8c9b61ca4f92ba31e8f7833316
Ekaterina <108363825+mckater@users.noreply.github.com>
2022-12-03 23:13:02 +0300

    c1

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = 'yandexlyceum_secret_key'
 DEFAULT_CITY = 'Moscow,Russia'
 WEATHER_API_KEY = '5d20b324861d406ab66183759221911'
-WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+WEATHER_URL = 'ht_tp://api.worldweatheronline.com/premium/v1/weather.ashx'
 # для удобства из любого расположения в разных ОС
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, ' .. ')
