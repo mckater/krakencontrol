a19efbfc44c0d1372771189f09f4317d6e8625ea
myfreer <my.free@mail.ru>
2023-03-14 18:33:17 +0300

    fixed Python 3.9 (empty population.db exists)

--- config.py
+++ config.py
@@ -3,6 +3,5 @@ use_reloader = False  # обязательно включен в режиме de
 # debug = True
 debug = False
 DEFAULT_CITY = 'Moscow,Russia'
-WEATHER_API_KEY = '8555e5165e564ce09b1164027221112'  # 500 запросов в сутки
-# WEATHER_API_KEY = '5d20b324861d406ab66183759221911'  # запасной, действует до 18 дек.
+WEATHER_API_KEY = '8555e5165e564ce09b1164027221112'  # закончился, сделать новый на 1 месяц
 WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
